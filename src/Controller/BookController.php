668ea82254e61f6af8a0c4754718c784e668e21a
Julio <julesnoir@hotmail.fr>
2022-01-31 11:21:56 +0100

    Show list of books and you can create new one

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends AbstractController
 {
@@ -21,18 +25,49 @@ class BookController extends AbstractController
     public function showBooks(ManagerRegistry $registry): Response
     {
         $books = $registry->getRepository(Book::class)->findAll();
-        dump($books);
-        return $this->render('book/index.html.twig', [
+        return $this->render('book/listing.html.twig', [
             'controller_name' => 'BookController',
+            'books' => $books
         ]);
     }
-    #[Route('/book/Create', name: 'book_create')]
-    public function createBook(ManagerRegistry $registry): Response
+    #[Route('/book/create', name: 'book_create')]
+    public function createBook(ManagerRegistry $registry,Request $request,EntityManagerInterface $event): Response
     {
-        $books = $registry->getRepository(Book::class)->findAll();
-        dump($books);
-        return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
+        $book = new Book();
+
+        $form = $this->createForm(BookType::class,$book);
+        $form->add("submit",SubmitType::class,["attr" => ["class" => "btn btn-primary"]]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $event->persist($book);
+            $event->flush();
+            return $this->redirectToRoute('book_listing');
+        }
+        
+        return $this->renderForm('book/create.html.twig', [
+            'controller_name' => 'Create Book',
+            'form' => $form
+        ]);
+    }
+    #[Route('/book/edit', name: 'book_edit')]
+    public function editBook(ManagerRegistry $registry,Request $request,EntityManagerInterface $event): Response
+    {
+        $book = new Book();
+
+        $form = $this->createForm(BookType::class,$book);
+        $form->add("submit",SubmitType::class,["attr" => ["class" => "btn btn-primary"]]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $event->persist($book);
+            $event->flush();
+            return $this->redirectToRoute('book_listing');
+        }
+        
+        return $this->renderForm('book/create.html.twig', [
+            'controller_name' => 'Create Book',
+            'form' => $form
         ]);
     }
 }
