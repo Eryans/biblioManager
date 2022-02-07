0c02de6c3673ce72051812bb4f756104b52ddf9a
Julio <julesnoir@hotmail.fr>
2022-02-07 15:47:57 +0100

    added things to do

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -11,17 +11,39 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title',TextType::class,["label" => "Title"])
-            ->add('author',TextType::class,[])
-            ->add('summary',TextAreaType::class,[])
+            ->add('title',TextType::class,["label" => "Title",
+            'constraints' => [
+                new Length([
+                    'min' => 1,
+                    'max' => 100
+                ]),
+            ]])
+            ->add('author',TextType::class,['constraints' => [
+                new Length([
+                    'min' => 1,
+                    'max' => 100
+                ]),
+            ]])
+            ->add('summary',TextAreaType::class,[
+            'constraints' => [
+                new Length([
+                    'min' => 2,
+                    'max' => 400
+                ])]])
             ->add('release_date',DateType::class, ['widget' => 'single_text',"label" => "release_date"])
-            ->add('category',TextType::class,[])
+            ->add('category',TextType::class,['constraints' => [
+                new Length([
+                    'min' => 2,
+                    'max' => 50
+                ])]])
             ->add('for_child',CheckboxType::class,['required' => false, 'label' => 'forChild'])
             ->add('quantity',NumberType::class,[])
         ;
